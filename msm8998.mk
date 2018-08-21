84c3f8c713e0b586269239ceb04ae87a146b5843
hzj5158888 <www1693303798@163.com>
2018-08-21 14:16:02 +0800

    gps and camera

--- msm8998.mk
+++ msm8998.mk
@@ -226,8 +226,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libgps.utils \
     libgnss \
-    libgnsspps \
-    liblocation_api
+    libgnsspps
 
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl-qti \
@@ -413,6 +412,10 @@ PRODUCT_PACKAGES += \
 # VNDK
 PRODUCT_PACKAGES += \
     vndk_package
+
+# Fix Gnss
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v27/arm64/arch-arm64-armv8-a/shared/vndk-core/android.hardware.gnss@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/android.hardware.gnss@1.0-v27.so
 
 # VR
 PRODUCT_PACKAGES += \
