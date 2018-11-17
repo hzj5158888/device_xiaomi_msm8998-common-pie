e6cc7ae15181b823c969188789f906b48d6df846
Vol Zhdanov <wight554@gmail.com>
2018-11-17 23:07:13 +0000

    msm8998-common: disable dolby dap and remove thermal
    * jasmine doesn't ship it
    * haven't faced any changes with it but extra logspam

--- msm8998.mk
+++ msm8998.mk
@@ -380,7 +380,7 @@ PRODUCT_BOOT_JARS += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.1-service.sagit
+    android.hardware.thermal@1.0-service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
